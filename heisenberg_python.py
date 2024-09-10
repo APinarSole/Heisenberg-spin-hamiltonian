f9960cf55a69086c45dff75377ad1dbf7b05eef6
Andres Pinar Sole <andrespinarsole@gmail.com>
2024-09-10 20:40:05 +0900

    Update heisenberg_python.py

--- heisenberg_python.py
+++ heisenberg_python.py
@@ -6,7 +6,7 @@ import seaborn as sns
 """
     Heisenberg model  H = gμ(S₁B+S₂B) + D₁Sz₁² + D₂Sz₂² + E₁(Sₓ₁² - Sᵧ₁²) + E₂(Sₓ₂² - Sᵧ₂²) + J S₁S₂
     
-    Calculate the spin Hamiltonian of two spin systems S1 and S2 using the Heisenberg model. This script can handle S=1/2,1,3/2 
+    Calculate the spin Hamiltonian of two spin systems S1 and S2 using the Heisenberg model. This script can handle any spin value 
     with magnetic anisotropy (E,D) and a vectorial magnetic field B. For more advanced calculations of two spin systems, check
     https://github.com/ManishMitharwall/Nickelocene_Spin_Sensor 
     # H= Hamiltonian operator. Take in to account: dim(H)=dim(S1)*dim(S2)
@@ -16,6 +16,8 @@ import seaborn as sns
     # J>0 favours antiferromagnetic states of the combined system (Eg. E(↑↑)>E(↑↓))
     # D>0 favours low Sz of a single spin system (Eg. E1(1)>E1(1/2))
     # The script also allows to study a single particle by making one of the spins S=0
+    # The script prints the states, its eigenvectors and the energies
+    
     
     Parameters:
     - Jmax: Echange coupling constant
@@ -34,7 +36,7 @@ import seaborn as sns
 
 # Define parameters
 spin1 = 1   # spin 1
-spin2 = 1  # spin 2
+spin2 = 0.5  # spin 2
 D1 = 4   # out of plane magnetic anisotropy 1 in meV
 D2 = 4   # out of plane magnetic anisotropy 2 in meV
 E1 = 0   # in plane magnetic anisotropy spin 1 in meV
@@ -43,108 +45,40 @@ B = [0,0,0]    # vectorial magnetic field in Tesla
 Jmax=2  # max. exchange coupling in meV
 
 def heisenberg(J, spin1, spin2,D1, D2,E1,E2, B):
-    # spin 0
-    x0=np.array([[0]])
-    y0=np.array([[0]])
-    z0=np.array([[0]])
-    base0=[['0']]
-        
-    # Spin 1/2 (2x2 matrices)
-    x_1_2 = 0.5*np.array([[0, 1], [1, 0]])  # Pauli X
-    y_1_2 = 0.5*np.array([[0, -1j], [1j, 0]])  # Pauli Y
-    z_1_2 = 0.5*np.array([[1, 0], [0, -1]])  # Pauli Z
-    base1_2 = [['1/2 '], ['-1/2 ']]
     
-    # Spin 1 (3x3 matrices)
-    sqrt2_inv = 1 / np.sqrt(2)  # 1/sqrt(2)
-    x_1 = sqrt2_inv * np.array([[0, 1, 0], [1, 0, 1], [0, 1, 0]])  # Pauli X for S=1
-    y_1 = sqrt2_inv * np.array([[0, -1j, 0], [1j, 0, -1j], [0, 1j, 0]])  # Pauli Y for S=1
-    z_1 = np.array([[1, 0, 0], [0, 0, 0], [0, 0, -1]])  # Pauli Z for S=1
-    base_1 = [['1 '], ['0 '], ['-1 ']]
-   
-    # Spin 3/2 (4x4 matrices)
-    sqrt3_inv = 1 / np.sqrt(3)  # 1/sqrt(3)
-    x_3_2 = sqrt3_inv * np.array([[0, 1, 0, 0],
-                                  [1, 0, 1, 0],
-                                  [0, 1, 0, 1],
-                                  [0, 0, 1, 0]])
+     
+    def pauli_matrix(s):  # Pauli matrices generator
+        s=float(s)
+        n = int(2*s+1)
+        sx=np.empty([n,n])
+        sy=np.empty([n,n],dtype=complex)
+        sz=np.empty([n,n])
+        for a in range(1,n+1):
+            for b in range(1,n+1):
+                sx[a-1,b-1]=( 0.5*((a==b+1) + (a+1==b))*np.sqrt((s+1)*(a+b-1)-a*b))
+                sy[a-1,b-1] =  1j*(0.5*((a==b+1) - (a+1==b))*np.sqrt((s+1)*(a+b-1)-a*b))
+                sz[a-1,b-1] = (s+1-a)*(a==b)
+        return sx,sy,sz
     
-    y_3_2 = sqrt3_inv * np.array([[0, -1j, 0, 0],
-                                  [1j, 0, -1j, 0],
-                                  [0, 1j, 0, -1j],
-                                  [0, 0, 1j, 0]])
-    
-    z_3_2 = np.array([[3/2, 0, 0, 0],
-                      [0, 1/2, 0, 0],
-                      [0, 0, -1/2, 0],
-                      [0, 0, 0, -3/2]])
-    base3_2 = [['3/2 '], ['1/2 '], ['-1/2 '], ['-3/2 ']]
-   
-    # Pauli for S=2
-    sqrt6_inv = 1 / np.sqrt(6)  # 1/sqrt(6)
-
-    x_2 = sqrt6_inv * np.array([[0, 1, 0, 0, 0],
-                                [1, 0, 1, 0, 0],
-                                [0, 1, 0, 1, 0],
-                                [0, 0, 1, 0, 1],
-                                [0, 0, 0, 1, 0]])
-    
-    y_2 = sqrt6_inv * np.array([[0, -1j, 0, 0, 0],
-                                [1j, 0, -1j, 0, 0],
-                                [0, 1j, 0, -1j, 0],
-                                [0, 0, 1j, 0, -1j],
-                                [0, 0, 0, 1j, 0]])
-    
-    z_2 = np.array([[2, 0, 0, 0, 0],
-                    [0, 1, 0, 0, 0],
-                    [0, 0, 0, 0, 0],
-                    [0, 0, 0, -1, 0],
-                    [0, 0, 0, 0, -2]])
-    base_2 = [['2 '], ['1 '], ['0 '], ['-1 '], ['-2 ']]
-    
-    def set_spin_matrices(spin):
-        if spin==0:
-            x1 = x0
-            y1 = y0
-            z1 = z0
-        elif spin == 0.5:
-            x1 = x_1_2
-            y1 = y_1_2
-            z1 = z_1_2
-        elif spin == 1:
-            x1 = x_1
-            y1 = y_1
-            z1 = z_1
-        elif spin == 1.5:
-            x1 = x_3_2
-            y1 = y_3_2
-            z1 = z_3_2
-        elif spin == 2:
-            x1 = x_2
-            y1 = y_2
-            z1 = z_2
-        
-        return x1, y1, z1
-
     def base(spin):
-        if spin==0:
-            base=base0
-        elif spin == 0.5:
-            base = base1_2
-        elif spin == 1:
-            base = base_1
-        elif spin == 1.5:
-            base = base3_2
-        elif spin == 2:
-            base = base_2
-        return base
-
+     if spin == 0:
+        base = [['0']]
+     elif spin == 0.5:
+        base = [['1/2'], ['-1/2']]
+     else:
+        base = [[f'{int(spin) - i} '] for i in range(int(2*spin) + 1)]
+     return base
+    
+    
+    
     base1 = np.array(base(spin1))
     base2 = np.array(base(spin2))
     
-    x1, y1, z1 = set_spin_matrices(spin1)
-    x2, y2, z2 = set_spin_matrices(spin2)
+        
+    x1, y1, z1 = pauli_matrix(spin1)
+    x2, y2, z2 = pauli_matrix(spin2)
     
+        
     S1 = [ np.kron(x1, np.eye(x2.shape[1])),  np.kron(y1, np.eye(y2.shape[1])),  np.kron(z1, np.eye(z2.shape[1]))]
     S2 = [ np.kron(np.eye(x1.shape[1]), x2),  np.kron(np.eye(y1.shape[1]), y2),  np.kron(np.eye(z1.shape[1]), z2)]
    
@@ -228,37 +162,49 @@ for col in ket_matrix.columns:
     associated_base = [base for base in ket_matrix.index[ket_matrix[col] != 0]]
     results[col] = list(zip(associated_base, np.round(non_zero_entries,2)))
 
-# Display results
+
+# Display eigenvalues (E0, E1, etc.)
+print("\nEnergies:")
+for i, col in enumerate(ket_matrix.columns):
+    eigenvalue = E[i]  # Assuming `E` contains the eigenvalues
+    print(f"E{i} = {eigenvalue} ")
+# Create labels for eigenvectors
+fila = [f'ψ{i}' for i in range(len(E))]
+
+# States
 print("\nStates (normalized):")
 for col in ket_matrix.columns:
     state_name = base_to_state[tuple(col)]
     state_values = " ".join([f"{value}({base_label})" for base_label, value in results[col]])
     print(f"{state_name} = {state_values}")
 
-
-# Create labels for eigenvectors
-fila = [f'ψ{i}' for i in range(len(E))]
-
 # Plot the eigenvalues
-plt.figure(figsize=(10, 6))
+if ket.shape[1]<=20:               # limits the plot to dimension 20, change it if you need
+    plt.figure(figsize=(12, 8))
+else:
+    plt.figure(figsize=(30, 18))
 for i in range(eigenvalues.shape[1]):
     plt.scatter(JJ, eigenvalues[:, i] - eigenvalues[:, 0],label=f'E-E0 {i}')
-plt.xlabel('J (meV)',fontsize=20)
-plt.ylabel('E-E0 (meV)',fontsize=20)
+plt.xlabel('J (meV)',fontsize=25)
+plt.ylabel('E-E0 (meV)',fontsize=25)
 plt.title(r'$B = ' + str(B) + r'\ \ S_1 = ' + str(spin1) + r'\ \ S_2 = ' + str(spin2) + 
-          r'\ \ D_1 = ' + str(D1) + r'\ \ D_2 = ' + str(D2) + '$', fontsize=20)
-plt.legend(fila, loc='upper left', bbox_to_anchor=(1, 1),fontsize=20)
+          r'\ \ D_1 = ' + str(D1) + r'\ \ D_2 = ' + str(D2) + '$', fontsize=25)
+plt.legend(fila, loc='upper left', bbox_to_anchor=(1, 1),fontsize=25)
 plt.grid(True)
-plt.xticks(fontsize=15)
-plt.yticks(fontsize=15)
+plt.xticks(fontsize=20)
+plt.yticks(fontsize=20)
 plt.show()
 
 # Create DataFrame for eigenvectors
 ket_matrix = pd.DataFrame(ket, columns=fila, index=base_tot)
 
-# Plot the eigenvectors 
-plt.figure(figsize=(12, 8))
-sns.heatmap(np.round(ket_matrix,2), cmap='viridis', annot=True, fmt='.2f', cbar=True,annot_kws={"size": 15})  # remove round to get the raw coefficients
+# Plot the eigenvectors
+if ket.shape[1]<=20:               # limits the plot to dimension 20, change it if you need
+    plt.figure(figsize=(12, 8))
+else:
+    plt.figure(figsize=(30, 18)) 
+plt.figure(figsize=(15, 10))
+sns.heatmap(np.round(ket_matrix,2), cmap='viridis', annot=True, fmt='.2f', cbar=True,annot_kws={"size": 20})  # remove round to get the raw coefficients
 plt.xlabel('States',fontsize=20)
 plt.ylabel(r'Autovectors Basis $|S_1, S_2\rangle$', fontsize=20)
 plt.title('Normalized coefficients',fontsize=20)
@@ -267,5 +213,7 @@ plt.xticks(ticks=np.arange(len(fila)) + 0.5, labels=fila, rotation=45,fontsize=2
 plt.yticks(ticks=np.arange(len(base_tot)) + 0.5, labels=base_tot, rotation=0, fontsize=20)
 plt.tight_layout()
 plt.show()
+
+
 
 
